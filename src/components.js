43baa56dcf03d61d10992418cdc187f47d5902c3
Colton <coltonj@hranswerlink.com>
2014-03-02 17:11:12 -0800

    Start to make gems slide aka swap

--- src/components.js
+++ src/components.js
@@ -16,9 +16,24 @@ Crafty.c('Tile', {
 Crafty.c('Gem', {
   init: function() {
     this.requires('Actor');
-    this.requires(this.possible_sprites[Math.floor(Math.random() * this.possible_sprites.length)]);
     this._globalZ = 1;
+    this.requires(this.possible_sprites[Math.floor(Math.random() * this.possible_sprites.length)]);
+
+    this.bind('TweenEnd', function() {
+      var props = {};
+      props[this.lastTweenAxis] = this.lastTweenDist;
+
+      this.tween(props, 500);
+      this.enableDrag();
+    });
+
+    this.bind('Dragging', function() {
+      this.disableDrag();
+      var dir = getDir(this);
+      swap(this, dir);
+    });
   },
+
   possible_sprites: [
     'spr_air',
     'spr_earth',
@@ -65,3 +80,66 @@ Crafty.c('Actor', {
     this.room = room;
   }
 });
+
+
+// Desc:   Grabs the direction a gem was moved.
+// Params:
+//         - Entity (Gem)
+// Return: Array Array [x_or_y_axis, postive_or_negative_1_for_direction_on_axis]
+
+function getDir(gem) {
+  var x    = gem.x;
+  var y    = gem.y;
+  var oldX = gem._oldX;
+  var oldY = gem._oldY;
+  var dir;
+
+  // No diagonal moving!
+  if(Math.abs(y-oldY) > Math.abs(x-oldX)) {
+    if(y > oldY) {
+      dir = ['y', 1];
+    } else if(y < oldY) {
+      dir = ['y', -1];
+    }
+  } else {
+    if(x > oldX) {
+      dir = ['x', 1];
+    } else if(x < oldX) {
+      dir = ['x', -1];
+    }
+  }
+
+  return dir;
+}
+
+
+// TODO:   Finish canMove
+// Desc:   Checks if there is an adjacent gem.
+// Params: 
+//         - Entity (Gem)
+//         - Array [x_or_y_axis, postive_or_negative_1_for_direction_on_axis]
+// Return: boolean
+
+function canMove(gem, dir) {
+  //Me need some code
+}
+
+//TODO:    Finish the swap!
+// Desc:   Swaps two gems placement based on direction 'dir'
+//         If no gem adjacent to the one that was dragged, return.
+// Params: 
+//         - Entity (Gem) 
+//         - Array [x_or_y_axis, positive_or_negative_1_for_direction_on_axis]
+// Return: void
+
+function swap(gem, dir) {
+  var axis    = dir[0];
+  var dist    = gem[axis] + (dir[1]*72);
+  var props   = {};
+
+  props[axis] = dist;
+  entity.lastTweenAxis = axis;
+  entity.lastTweenDist = entity[axis];
+
+  entity.tween(props, 500);
+}
